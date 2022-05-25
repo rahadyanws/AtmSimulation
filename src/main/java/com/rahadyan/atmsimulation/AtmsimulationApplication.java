b950983fd0d63cbb6c03038788d183359f2a79a0
Rahadyan Widhi Saktyaji <rahadyanws@gmail.com>
2022-05-25 11:16:03 +0700

    init commit

---
+++ src/main/java/com/rahadyan/atmsimulation/AtmsimulationApplication.java
@@ -0,0 +1,13 @@
+package com.rahadyan.atmsimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmsimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmsimulationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rahadyan/atmsimulation/AtmsimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahadyan.atmsimulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmsimulationApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
